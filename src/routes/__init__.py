0551c6c5d9233dc9b2675e7323ba4b13a9e263d6
attwo <anntakefour@gmail.com>
2023-03-28 18:56:13 +0300

    feat(routes): Добавлен эндпоинт для расчета VLP

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,13 +1,30 @@
 from fastapi import APIRouter
+from src.db import get_session
 from src.models.models import VlpCalcRequest, VlpCalcResponse
 from src.routes.queries import save_init_data, save_vlp_data, get_check_well_data_exists, get_check_vlp_exists
+from src.utils import get_dict_hash
+
 
 main_router = APIRouter(prefix="/vlp", tags=["VLP"])
 
 
 @main_router.post("/calc", response_model=VlpCalcResponse)
-def calc_vlp(vlp_in: VlpCalcRequest):
+async def calc_vlp(vlp_in: VlpCalcRequest):
     """Расчёт VLP по исходным данным и сохранение в Базу"""
     # функция считающая VLP
     from src.calculations.vlp import calc_vlp as vlp_calculation  # noqa
-    pass
+
+    vlp_in_dict = vlp_in.dict()
+    well_data_id = get_dict_hash(vlp_in_dict)
+    vlp = None
+
+    with get_session() as session:
+        if not get_check_well_data_exists(session, well_data_id):
+            save_init_data(session, vlp_in_dict, well_data_id)
+
+        vlp = get_check_vlp_exists(session, well_data_id)
+        if not vlp:
+            vlp = await vlp_calculation(**vlp_in_dict)
+            save_vlp_data(session, vlp, well_data_id)
+
+    return vlp
